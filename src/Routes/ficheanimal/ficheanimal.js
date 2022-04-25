0e83021ce0c5fceeb5f20720b578b73048c7717c
Charles Lambret <79523370+CharlesLambret@users.noreply.github.com>
2022-04-25 17:02:07 +0200

    correction

--- src/Routes/ficheanimal/ficheanimal.js
+++ src/Routes/ficheanimal/ficheanimal.js
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material';
-import { where } from 'firebase/firestore';
+import { getDoc, where } from 'firebase/firestore';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -8,67 +8,65 @@ import { Navbar } from '../../navbar/navbar.js';
 
 export async function Ficheanimal() {
     const [animals, setAnimals] = useState([]);
-
+   
     useEffect(() => {async function getAnimaux() {
-        const q = query(collection(db, 'Animaux-a-adopter'), where("Espece","==","chien"));
-        const AnimauxSnapshot = await getDocs(q).then(reponse => {}).catch(error => console.log(error.message));
-        const AnimauxList = AnimauxSnapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
-        setAnimals (AnimauxList);
+        const db = getFirestore(appfirebase);
+        const AnimauxCol = collection(db, 'Animaux-a-adopter');
+        const AnimauxSnapshot = await getDoc(AnimauxCol, location.state.id);
+        setAnimals ({id:AnimauxSnapshot.id, ...AnimauxSnapshot.data()});
     }})
 const navigate = useNavigate();
-const ClickMessage = navigate("/inscription");
+const location = useLocation();
+console.log(location.state.animal);
 
 return( 
-<div>
-    {animals.map(({animals}) => 
-    <div>
-        <div>
-            <h2>{animals.nom}</h2>
-            <p>{animals.Race}</p>
-        </div>
+               <div>
+                    <div>
+                        <h2>{animals.nom}</h2>
+                        <p>{animals.Race}</p>
+                    </div>
 
-        <div className="caract">
-            <div>
-                <p>{animals.Âge}</p>
-            </div>
-            <div>
-                <p>Sexe</p>
-                <p>{animals.Sexe}</p>
-            </div>
+                    <div className="caract">
+                        <div>
+                            <p>{animals.Âge}</p>
+                        </div>
+                        <div>
+                            <p>Sexe</p>
+                            <p>{animals.Sexe}</p>
+                        </div>
 
-            <div>
-                <img></img>
-                <p>Couleur</p>
-                <p>{animals.Couleur}</p>
-            </div>
+                        <div>
+                            <img></img>
+                            <p>Couleur</p>
+                            <p>{animals.Couleur}</p>
+                        </div>
 
-            <div>
-                <img></img>
-                <p>Taille</p>
-                <p>{animals.Taille}</p>
-            </div>
+                        <div>
+                            <img></img>
+                            <p>Taille</p>
+                            <p>{animals.Taille}</p>
+                        </div>
 
-        </div>
+                    </div>
 
-        <div className="longb">
-            <div>
-                <p>{animals.Localisation}</p>
-            </div>
-        </div>
+                    <div className="longb">
+                        <div>
+                            <p>{animals.Localisation}</p>
+                        </div>
+                    </div>
 
-        <div>
-            <p>{animals.Description}</p>
-        </div>
+                    <div>
+                        <p>{animals.Description}</p>
+                    </div>
 
-        <div>
-        <p>Pour pouvoir contacter l’annonceur <br></br>il va falloir créer un compte Adora </p>
-        </div>
+                    <div>
+                    <p>Pour pouvoir contacter l’annonceur <br></br>il va falloir créer un compte Adora </p>
+                    </div>
 
-        <div className="bigbtn">
-            <Button onClick={ClickMessage}>Contacter l'annonceur</Button>
-        </div>
-    </div>)}
-</div>
+                    <div className="bigbtn">
+                        <Button>Contacter l'annonceur</Button>
+                    </div>
+                </div>
+           )       
 
-)
-}
+};
